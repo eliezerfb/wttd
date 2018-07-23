448cb797e02a73356baadcd902309bcf86d08ffe
Eliézer Bourchardt <eliezerfb@gmail.com>
2018-07-23 09:41:02 -0300

    Revert "order list"
    This reverts commit 015beace8d3343525de5b48b463f3027b2c66798.
    
    reverte para commit anterior

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -50,8 +50,7 @@ class SubscriptionFormTest(TestCase):
     def test_without_phone_and_invalid_email(self):
         """Email and phone are optional, but one must be informed"""
         form = self.make_validate_form(email='xpto', phone='')
-        self.assertListEqual(sorted(['__all__', 'email']),
-                             sorted(list(form.errors)))
+        self.assertListEqual(['__all__', 'email'], list(form.errors))
 
 
     def assertFormErrorCode(self, form, field, code):
